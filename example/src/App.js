9249d53b151b8acabe84e5318b922200a1baa110
LogicalOgbonna <arinzeogbo@gmail.com>
2021-02-17 05:24:27 +0100

    style(component): added styling to the component

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,24 @@
 import React from 'react'
 
-import { ExampleComponent } from 'mapper'
+import { FileToJsonMaper } from 'mapper'
 import 'mapper/dist/index.css'
-
+const keys = {
+  ours: [
+      { value: "surname", label: "Surname" },
+      { value: "otherNames", label: "Other Names" },
+      { value: "email", label: "email" },
+      { value: "password", label: "Password" },
+  ],
+  theirs: [
+      { value: "bankName", label: "BankName" },
+      { value: "tellerNumber", label: "Teller" },
+      { value: "tellerDate", label: "Date" },
+      { value: "status", label: "Status" },
+      { value: "phone", label: "Phone" },
+  ]
+}
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <FileToJsonMaper keys={keys.ours} mapsTo={keys.theirs} />
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,51 @@
+import PropTypes from 'prop-types'
 import React from 'react'
 import styles from './styles.module.css'
+import Select from 'react-select'
+import { Card } from 'antd'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const FileToJsonMaper = ({ keys, mapsTo }) => {
+  if (!keys || !mapsTo) return null
+  return (
+    <div className={styles.file_to_json_style}>
+      <Card className={styles.file_to_json_card_style}>
+        <p className={styles.file_to_json_card_text_style}>
+          Please select a key
+        </p>
+        <Select
+          className='basic-single  text-capitalize file_to_json_padding_top'
+          classNamePrefix='select'
+          // isDisabled={true}
+          isLoading={false}
+          isClearable={false}
+          name='keys'
+          options={keys}
+        />
+        <Select
+          className={styles.file_to_json_padding_top}
+          classNamePrefix='select'
+          // isDisabled={true}
+          isLoading={false}
+          isClearable={false}
+          name='mapsTo'
+          options={mapsTo}
+        />
+      </Card>
+    </div>
+  )
+}
+
+FileToJsonMaper.propTypes = {
+  keys: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string,
+      name: PropTypes.string
+    })
+  ).isRequired,
+  mapsTo: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string,
+      name: PropTypes.string
+    })
+  ).isRequired
 }
