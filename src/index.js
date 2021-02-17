1a0872afa9af1597be885d2ca896e2afc2ef005a
LogicalOgbonna <arinzeogbo@gmail.com>
2021-02-17 05:29:46 +0100

    style(component): added styling to the component

--- src/index.js
+++ src/index.js
@@ -3,35 +3,14 @@ import React from 'react'
 import styles from './styles.module.css'
 import Select from 'react-select'
 import { Card } from 'antd'
+import FileReader from './components/FileMapper'
 
 export const FileToJsonMaper = ({ keys, mapsTo }) => {
   if (!keys || !mapsTo) return null
   return (
-    <div className={styles.file_to_json_style}>
-      <Card className={styles.file_to_json_card_style}>
-        <p className={styles.file_to_json_card_text_style}>
-          Please select a key
-        </p>
-        <Select
-          className='basic-single  text-capitalize file_to_json_padding_top'
-          classNamePrefix='select'
-          // isDisabled={true}
-          isLoading={false}
-          isClearable={false}
-          name='keys'
-          options={keys}
-        />
-        <Select
-          className={styles.file_to_json_padding_top}
-          classNamePrefix='select'
-          // isDisabled={true}
-          isLoading={false}
-          isClearable={false}
-          name='mapsTo'
-          options={mapsTo}
-        />
-      </Card>
-    </div>
+    <>
+      <FileReader />
+    </>
   )
 }
 
