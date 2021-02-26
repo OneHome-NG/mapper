507f9df7abbd182ee30564f57143b8ba94b66af3
LogicalOgbonna <arinzeogbo@gmail.com>
2021-02-26 11:13:26 +0100

    feat(csv/excel): successfully returning a json file back after successfully mapping them to passsed keys

--- example/src/App.js
+++ example/src/App.js
@@ -2,23 +2,14 @@ import React from 'react'
 
 import { FileToJsonMaper } from 'mapper'
 import 'mapper/dist/index.css'
-const keys = {
-  ours: [
-      { value: "surname", label: "Surname" },
-      { value: "otherNames", label: "Other Names" },
-      { value: "email", label: "email" },
-      { value: "password", label: "Password" },
-  ],
-  theirs: [
-      { value: "bankName", label: "BankName" },
-      { value: "tellerNumber", label: "Teller" },
-      { value: "tellerDate", label: "Date" },
-      { value: "status", label: "Status" },
-      { value: "phone", label: "Phone" },
-  ]
-}
+const keys = [
+  { value: "surname", label: "Surname" },
+  { value: "otherNames", label: "Other Names" },
+  { value: "email", label: "email" },
+  { value: "password", label: "Password" },
+]
 const App = () => {
-  return <FileToJsonMaper keys={keys.ours} mapsTo={keys.theirs} />
+  return <FileToJsonMaper keys={keys} onComplete={(data) => console.log(data)} display />
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,16 +1,81 @@
 import PropTypes from 'prop-types'
-import React from 'react'
-import styles from './styles.module.css'
-import Select from 'react-select'
-import { Card } from 'antd'
-import FileReader from './components/FileMapper'
+import React, { useState } from 'react'
+import FileReader from './components/FileReader'
+import FileMapper from './components/FileMapper'
 
-export const FileToJsonMaper = ({ keys, mapsTo }) => {
-  if (!keys || !mapsTo) return null
+export const FileToJsonMaper = ({ keys, onComplete }) => {
+  const initialState = {
+    fileReader: true,
+    fileMapper: false,
+    displayFile: false
+  }
+  const initialFile = {
+    keys,
+    mapsTo: [],
+    data: []
+  }
+  const [stage, setStage] = useState(initialState)
+  const [file, setFile] = useState(initialFile)
+  const [mapper, setMapper] = useState([])
+  const onChange = (data) => {
+    setFile({ ...file, mapsTo: data.headers, data: data.body })
+    setStage({
+      ...stage,
+      fileReader: false,
+      fileMapper: true
+    })
+  }
+  const onBackToFileReader = () => {
+    setFile(initialFile)
+    setStage({
+      ...stage,
+      fileReader: true,
+      fileMapper: false
+    })
+  }
+  const onMapperChange = ({ rowIndex, columnIndex, keys, mapTo }) => {
+    const index = mapper.findIndex((map) => map.columnIndex === columnIndex)
+    if (index >= 0) {
+      const map = mapper[index]
+      const newMap = {
+        ...map,
+        keys: keys || map.keys,
+        mapTo: mapTo || map.mapTo,
+        rowIndex: isNaN(rowIndex) ? map.rowIndex : rowIndex
+      }
+      mapper[index] = newMap
+      return setMapper([...mapper])
+    }
+    return setMapper([...mapper, { rowIndex, keys, mapTo, columnIndex }])
+  }
+  const onContinue = () => {
+    const newMapper = []
+
+    const temp1Keys = {}
+    mapper.forEach((map) => {
+      temp1Keys[map.keys] = map.rowIndex
+    })
+    file.data.forEach((value) => {
+      const stupidObject = {}
+      Object.keys(temp1Keys).forEach((v) => {
+        stupidObject[v] = value[temp1Keys[v]]
+      })
+      newMapper.push(stupidObject)
+    })
+    onComplete(newMapper)
+  }
   return (
-    <>
-      <FileReader />
-    </>
+    <React.Fragment>
+      <FileReader visible={stage.fileReader} onChange={onChange} />
+      <FileMapper
+        onContinue={onContinue}
+        onChange={onMapperChange}
+        onBackToFileReader={onBackToFileReader}
+        visible={stage.fileMapper}
+        keys={file.keys}
+        mapsTo={file.mapsTo}
+      />
+    </React.Fragment>
   )
 }
 
@@ -20,11 +85,11 @@ FileToJsonMaper.propTypes = {
       label: PropTypes.string,
       name: PropTypes.string
     })
-  ).isRequired,
-  mapsTo: PropTypes.arrayOf(
-    PropTypes.shape({
-      label: PropTypes.string,
-      name: PropTypes.string
-    })
-  ).isRequired
+  ),
+  onComplete: PropTypes.func.isRequired,
+  display: PropTypes.bool
+}
+
+FileToJsonMaper.defaultProps = {
+  display: false
 }
